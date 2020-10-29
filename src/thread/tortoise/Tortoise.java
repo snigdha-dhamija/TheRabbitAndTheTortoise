ebda5a750902f0e802ff9322127eff5b80a76b30
harsh822 <harsh.gautam_cs19@gla.ac.in>
2020-10-29 08:22:38 +0530

    add constructor and getter method in Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,5 +1,16 @@
 package thread.tortoise;
 
-public class Tortoise extends Thread {
+import thread.rabbit.Rabbit;
 
+public class Tortoise extends Thread {
+    public static final int MILESTONES = 5;
+    private final Thread tortoise;
+
+    public Tortoise(Thread tortoise) {
+        this.tortoise = new Thread(this,"Torotise");
+    }
+
+    public Thread getTortoise() {
+        return tortoise;
+    }
 }
