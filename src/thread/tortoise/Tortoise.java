9482b1484248c4b1b7a85829d6a81823624cce3f
snigdha-dhamija <67826444+snigdha-dhamija@users.noreply.github.com>
2020-10-30 15:17:25 +0530

    Update Tortoise.java
    Class tortoise

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,12 @@
+
+
 package thread.tortoise;
 
-public class Tortoise {
+import javax.swing.*;
+
+public class Tortoise extends Slow {
+
+    public static final int MILESTONES = 5;
+
+    private final Thread tortoise;
 }
